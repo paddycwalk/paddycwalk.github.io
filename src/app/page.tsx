6e6b82d1ca14211a1046d2ecc0f298a085f93546
patrick.schott <patrick.schott@kittelberger.de>
2024-05-24 16:25:16 +0200

    [main] escape

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,9 +5,9 @@ export default function Home() {
     <main className={styles.main}>
       <div className={styles.Intro}>
         <h1>
-          <span className={styles.big}>Hello</span>, I'm Patrick.
+          <span className={styles.big}>Hello</span>, I&apos;m Patrick.
         </h1>
-        <h1>I'm a Frontend Developer.</h1>
+        <h1>I&apos;m a Frontend Developer.</h1>
         <h4 className={styles.stay}>- stay tuned -</h4>
       </div>
     </main>
